a5ab6b760ba8d8999400bfe130a51d94fb573ca7
Adenou Junior <adenoujunior@gmail.com>
2023-05-02 18:39:17 -0300

    Update FloorBinarySearchImpl.java

--- R04-02-Rot-KLargestQuickSelectFloor-environment/src/main/java/problems/FloorBinarySearchImpl.java
+++ R04-02-Rot-KLargestQuickSelectFloor-environment/src/main/java/problems/FloorBinarySearchImpl.java
@@ -4,8 +4,23 @@ public class FloorBinarySearchImpl implements Floor {
 
 	@Override
 	public Integer floor(Integer[] array, Integer x) {
-		// TODO implement your code here
-		throw new UnsupportedOperationException("Not implemented yet!");
-	}
+		int left = 0;
+        int right = array.length - 1;
+        Integer floor = null;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (array[mid].compareTo(x) == 0) {
+                return array[mid];
+            } else if (array[mid] < x) {
+                floor = array[mid];
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return floor;
+    }
 
 }
